6ba8bbd520851b95e06f6cc4670f7c8c973271a2
Andrez18 <ieca.andresgomez@gmail.com>
2025-08-15 10:33:11 -0500

    Agregando config para fuentes colores y poder utilizar tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
