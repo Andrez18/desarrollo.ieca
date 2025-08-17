1b36ec093f80ca075a91617293eb1f398a66155a
Andrez18 <ieca.andresgomez@gmail.com>
2025-08-17 17:49:53 -0500

    Modificando configuración de los proyectos y agregando un archivo de proyecto de más para pruebas

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,17 @@
 import { defineCollection, z } from "astro:content";
 // z --> Utilizamos zos para validar los datos que se piden en la colección (Los markdowns)
-
 const projects = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        members: z.string(),
-        img: z.string(),
-        year: z.number(),
-        description: z.string(),
-        get: z.object({
-            gitHub: z.string().url(),
-            site: z.string().url()
-        }), 
-    })
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    img: z.string(),
+    developedyear: z.number(),
+    description: z.string(),
+    get: z.object({
+      git: z.string().url(),
+      site: z.string().url(),
+    }),
+  })
 })
 
-export const collections = { projects };
+export const collections = { projects }
